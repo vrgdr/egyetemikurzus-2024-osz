dc9e9eecb312835f2cb5c10d06df21670d2b4f19
Varga Dóra <h379039@stud.u-szeged.hu>
2024-11-23 13:53:00 +0100

    draw game board

--- JCY9ZR/Program.cs
+++ JCY9ZR/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Clear();
+        Console.CursorVisible = false;
+
+        SnakeGame game = new SnakeGame(40, 20);
+        game.Run();
+    }
+}
---
+++ JCY9ZR/SnakeGame.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+public class SnakeGame
+{
+    private int width;
+    private int height;
+
+    public SnakeGame(int width, int height)
+    {
+        this.width = width;
+        this.height = height;
+    }
+
+    public void Run()
+    {
+        DrawGameBoard();
+    }
+
+    private void DrawGameBoard()
+    {
+        for (int y = 0; y < height; y++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                if (x == 0 || x == width - 1 || y == 0 || y == height - 1)
+                    Console.Write("#");
+                else
+                    Console.Write(" ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
